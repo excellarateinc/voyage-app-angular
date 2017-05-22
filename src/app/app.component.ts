3c6fd74eed4d0f30de180a30a7face264f41af85
Jon Raymer <jon.raymer@lssinc.com>
2017-05-21 22:20:20 -0500

    Responsive sidenav.
    Will want to push this into a sidebar component/service of some sort,
    but POC working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewChild, Renderer2 } from '@angular/core';
+import { MdSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  @ViewChild('sidenav') sidenav: MdSidenav;
+  isMobile = false;
+  navMode = 'side';
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.checkWindowWidth();
+    this.renderer.listen(window, 'resize', (event) => {
+      this.checkWindowWidth();
+    });
+  }
+
+  private checkWindowWidth(): void {
+    this.isMobile = window.innerWidth <= 768;
+    if (this.isMobile) {
+      this.navMode = 'over';
+      this.sidenav.close();
+    } else {
+      this.navMode = 'side';
+      this.sidenav.open();
+    }
+  }
+}
