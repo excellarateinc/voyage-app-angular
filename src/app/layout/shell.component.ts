46f582a3a3af42da5167adea1ccd6da746160b7f
Jon Raymer <jon.raymer@lssinc.com>
2017-06-25 11:48:02 -0500

    Move to flex-layout instead of renderer2

--- src/app/layout/shell.component.ts
+++ src/app/layout/shell.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, Renderer2, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, Input } from '@angular/core';
 import { MdSidenav } from '@angular/material';
+import { MediaChange, ObservableMedia } from '@angular/flex-layout';
+import { Subscription } from 'rxjs/Subscription';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
@@ -7,26 +9,27 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   selector: 'app-shell',
   templateUrl: './shell.component.html'
 })
-export class ShellComponent implements OnInit {
+export class ShellComponent implements OnInit, OnDestroy {
   isMobile: boolean;
   isAuthenticated: boolean;
   @ViewChild('sidebar') sidebar: SidebarComponent;
+  private watcher: Subscription;
 
-  constructor(private renderer: Renderer2, private authenticationService: AuthenticationService) { }
+  constructor(private authService: AuthenticationService, private media: ObservableMedia) { }
 
   ngOnInit(): void {
-    this.isAuthenticated = this.authenticationService.getToken() != null;
-    this.checkWindowWidth();
-    this.renderer.listen(window, 'resize', (event) => {
-      this.checkWindowWidth();
+    this.isAuthenticated = this.authService.getToken() != null;
+    this.isMobile = this.media.isActive('xs') || this.media.isActive('sm');
+    this.watcher = this.media.subscribe((change: MediaChange) => {
+      this.isMobile = change.mqAlias === 'xs' || change.mqAlias === 'sm';
     });
+  }
+
+  ngOnDestroy(): void {
+    this.watcher.unsubscribe();
   }
 
   onToggleSidebar(): void {
     this.sidebar.toggle();
-  }
-
-  private checkWindowWidth(): void {
-    this.isMobile = window.innerWidth <= 768;
   }
 }
