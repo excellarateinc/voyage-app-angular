8b98ea952103769eac023f3fa1815aecfe68e93b
Jon Raymer <jon.raymer@lssinc.com>
2017-07-29 11:32:12 -0500

    Setting up QA build

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  API_URL: 'http://qa-api-ms.voyageframework.com/api/v1',
+  SERVER_URL: 'http://qa-api-ms.voyageframework.com',
+  OAUTH_REDIRECT_URL: 'http://qa-app-ms.voyageframework.com',
+  OAUTH_CLIENT_ID: 'client-super'
+};
