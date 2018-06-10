77e424979b5d4e9a07ef35f1c26444b159564e8b
Jon Raymer <jon.raymer@lssinc.com>
2018-06-10 12:54:23 -0500

    unit tests for validator and component

--- src/app/profile/confirm-password.validator.ts
+++ src/app/profile/confirm-password.validator.ts
@@ -3,6 +3,9 @@ import { AbstractControl, ValidationErrors, FormGroup } from '@angular/forms';
 export class ConfirmPasswordValidator {
   static MatchPassword(confirmNewPasswordControl: AbstractControl): ValidationErrors | null {
     const form = confirmNewPasswordControl.parent as FormGroup;
+    if (!form) {
+      return null;
+    }
     const newPassword = form.get('newPassword').value;
     const confirmPassword = confirmNewPasswordControl.value;
     if (!confirmPassword || newPassword !== confirmPassword) {
