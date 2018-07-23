f7a260d681f729903b634f509da8860d7af299d8
roblouie <rob.j.louie@gmail.com>
2018-07-23 13:56:26 -0500

    Fix accidentally removed import

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,12 +4,14 @@ import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { ExamplesModule } from '../examples/examples.module';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
     AngularMaterialModule,
     DashboardRoutingModule,
+    FlexLayoutModule,
     ExamplesModule,
   ],
   declarations: [DashboardComponent]
