f84e608ca53b3b6f0154a0adb45599d60d26e4bc
Adam Eslinger <adam.eslinger@gmail.com>
2018-07-30 16:14:13 -0500

    Adds to code coverage

--- src/app/core/user/user.service.spec.ts
+++ src/app/core/user/user.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { UserService } from './user.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { User } from './user.model';
 
 describe('UserService', () => {
   beforeEach(() => {
@@ -16,9 +17,62 @@ describe('UserService', () => {
     expect(service).toBeTruthy();
   }));
 
-  describe('when calling sendCode()', () => {
-    it('should call the send code API endpoint',
+  describe('when calling getUsers()', () => {
+    it('should call the users API endpoint',
       inject([UserService, HttpTestingController], (service: UserService, httpMock: HttpTestingController) => {
-    }));
+        service.getUsers().subscribe();
+        const req = httpMock.expectOne(request => request.url.includes('/users'));
+        expect(req.request.method).toEqual('GET');
+      }));
+  });
+
+  describe('when calling getCurrentUser()', () => {
+    it('should call the user API endpoint',
+      inject([UserService, HttpTestingController], (service: UserService, httpMock: HttpTestingController) => {
+        service.getCurrentUser().subscribe();
+        const req = httpMock.expectOne(request => request.url.includes('/profiles/me'));
+        expect(req.request.method).toEqual('GET');
+      }));
+
+    it('should return the existing user if it exists',
+      inject([UserService, HttpTestingController], (service: UserService, httpMock: HttpTestingController) => {
+        let expectedUser = new User()
+        service.currentUser = expectedUser;
+        service.getCurrentUser().subscribe((actualUser: User) => {
+          expect(expectedUser).toBe(actualUser);
+        });
+
+        httpMock.expectNone(request => request.url.includes('/profiles/me'));
+      }));
+
+      it('should call the user API endpoint even if it has an existing user if forced',
+      inject([UserService, HttpTestingController], (service: UserService, httpMock: HttpTestingController) => {
+        let expectedUser = new User()
+        service.currentUser = expectedUser;
+        service.getCurrentUser(true).subscribe((actualUser: User) => {
+          expect(expectedUser).not.toBe(actualUser);
+        });
+
+        const req = httpMock.expectOne(request => request.url.includes('/profiles/me'));
+        expect(req.request.method).toEqual('GET');
+      }));
+  });
+
+  describe('when calling toggleStatus()', () => {
+    it('should call the user account-status API endpoint',
+      inject([UserService, HttpTestingController], (service: UserService, httpMock: HttpTestingController) => {
+        service.toggleStatus(null, null).subscribe();
+        const req = httpMock.expectOne(request => request.url.includes('/account-status'));
+        expect(req.request.method).toEqual('PUT');
+      }));
+  });
+
+  describe('when calling updateProfile()', () => {
+    it('should call the profile API endpoint',
+      inject([UserService, HttpTestingController], (service: UserService, httpMock: HttpTestingController) => {
+        service.updateProfile(null).subscribe();
+        const req = httpMock.expectOne(request => request.url.includes('/profiles/me'));
+        expect(req.request.method).toEqual('PUT');
+      }));
   });
 });
---
+++ src/app/notifications/web-notification.service.spec.ts
@@ -0,0 +1,63 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { WebNotificationsService } from './web-notifications.service';
+
+describe('WebNotificationsService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        WebNotificationsService
+      ]
+    });
+  });
+
+  describe('when calling requestPermissions()', () => {
+    it('should do nothing if no Notification property on window',
+      inject([WebNotificationsService], (service: WebNotificationsService) => {
+        var result = service.requestPermission();
+        expect(result).toBeUndefined();
+        expect(service['permission']).toBeUndefined();
+      }));
+
+    it('should call Notification.requestPermission if permission not denied',
+      inject([WebNotificationsService], (service: WebNotificationsService) => {
+        window['Notification'] = { requestPermission: (fn) => fn('allowed') };
+        service.requestPermission();
+        expect(service['permission']).toBe('allowed');
+      }));
+
+    it('should call not Notification.requestPermission if permission denied',
+      inject([WebNotificationsService], (service: WebNotificationsService) => {
+        window['Notification'] = { requestPermission: (fn) => fn('denied') };
+        spyOn(window['Notification'], 'requestPermission');
+        service['permission'] = 'denied';
+        service.requestPermission();
+        expect(window['Notification'].requestPermission).not.toHaveBeenCalled();
+      }));
+  });
+
+  describe('when calling displayNotification()', () => {
+    it('should call navigator.serviceWorker.register when permission not denied',
+      inject([WebNotificationsService], (service: WebNotificationsService) => {
+        let mockReg = { showNotification: () => { } };
+        spyOn(mockReg, 'showNotification');
+        spyOn(window.navigator.serviceWorker, 'register').and.returnValue({ then: (fn) => { fn(mockReg) } });
+        service['permission'] = 'allowed';
+        service.displayNotification('', '');
+        expect(window.navigator.serviceWorker.register).toHaveBeenCalled();
+        expect(mockReg.showNotification).toHaveBeenCalled();
+      }));
+  });
+
+  describe('when calling displayNotification()', () => {
+    it('should not call navigator.serviceWorker.register when permission is denied',
+      inject([WebNotificationsService], (service: WebNotificationsService) => {
+        spyOn(window.navigator.serviceWorker, 'register').and.returnValue({ then: () => { } });
+        service['permission'] = 'denied';
+        service.displayNotification('', '');
+        expect(window.navigator.serviceWorker.register).not.toHaveBeenCalled();
+      }));
+  });
+});
